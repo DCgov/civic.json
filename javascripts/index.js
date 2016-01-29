672bd6471e2efe32ca447bf217796b90eeee10f0
Emanuel Feld <emanuelfeld@users.noreply.github.com>
2016-01-28 23:31:01 -0500

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,3 @@
-$.getJSON("./civic.json", function(json) {
+$.getJSON("https://raw.githubusercontent.com/DCgov/civic.json/master/schema.json", function(json) {
     $("#example_json").append(JSON.stringify(json, null, "  "));
 });
